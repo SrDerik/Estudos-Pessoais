6114e60ab977f856cdccad5fe17d510ffd43f28f
SrDerik <deriksoares217@gmail.com>
2025-12-01 00:15:53 -0300

    Aprendizado

---
+++ C/Curso C- Udemy/Modulo 2/Aula 5/CursoUDEMY-SwitchCase.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+ int valor = 4;
+    switch (valor) {
+      case 1:
+      printf("Valor 1\n");
+      break;
+   
+      case 2:
+      printf("Valor 2\n");
+      break;
+   
+      case 3: 
+      printf("Valor 3\n");
+      break;
+
+     default:
+     printf("Valor invalido");
+     break;
+    }
+}
