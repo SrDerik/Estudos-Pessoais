cfaf2262e8edf51736c8264a67ad396337c610a8
SrDerik <deriksoares217@gmail.com>
2025-12-15 22:48:29 -0300

    Arrumando erro no Ex06

--- C/Curso C- Udemy/Modulo 2/Exercicios 3/CursoUDEMY-Ex06.c
+++ C/Curso C- Udemy/Modulo 2/Exercicios 3/CursoUDEMY-Ex06.c
@@ -8,8 +8,8 @@ int main(){
    scanf("%d", &n);
    printf("\n");
    
-   for (int i = n; i != 1; i--){
-    for (int j = i; j != 1 ; j--){
+   for (int i = n; i >= 1; i--){
+    for (int j = 1; j < i ; j++){
      printf("%d",j);
     }
       printf("%d\n",i);
