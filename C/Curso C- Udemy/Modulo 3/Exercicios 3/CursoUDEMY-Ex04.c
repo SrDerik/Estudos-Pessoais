d25585556870754edc8dcbbe5b1f6f89ec608d00
SrDerik <deriksoares217@gmail.com>
2026-01-01 23:59:22 -0300

    Ex04

---
+++ C/Curso C- Udemy/Modulo 3/Exercicios 3/CursoUDEMY-Ex04.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+    
+    char nomes [5] [21];
+    char caractere = '\0';
+ 
+    printf("Digite o caractere escolhido:");
+    scanf(" %c", &caractere);
+
+    for (int i = 0; i < 5; i++) {
+        printf("Digite o %dº nome:", i+1);
+        scanf("%s", nomes[i]);
+    }
+    
+    for (int i = 0; i < 5; i++){
+       for (int j = 0; j < 20; j++){
+              if (nomes[i][j] == caractere){
+                printf("%s contém o caractere\n", nomes[i]);
+                break;
+            }
+       }
+          
+    }
+    
+}
